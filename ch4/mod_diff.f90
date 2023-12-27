c54362b13a0f2c7995a65d642d99a82e25541e90
StoatPower <StoatPower@users.noreply.github.com>
2023-12-27 18:47:47 -0500

    wrap up ch 4; more realistic physics w PDE using water height and water velocity

--- ch4/mod_diff.f90
+++ ch4/mod_diff.f90
@@ -8,13 +8,30 @@ contains
    ! applies the periodic boundary condition on the left (element on left edge of domain)
    ! because we're applying periodic (cyclic) boundary conditions and then
    ! computes the finite difference of input array
-   pure function diff(x) result(dx)
+   ! This is the UPWIND finite difference, meaning it is the difference in
+   ! ONE direction, as it is the diff between the current element and the
+   ! previous element
+   pure function diff_upwind(x) result(dx)
       real(real32), intent(in) :: x(:)  ! assumed-shape real array as input argument
       real(real32) :: dx(size(x))       ! the result will be a real array of the same size as x
       integer(int32) :: im
       im = size(x)
-      dx(1) = x(1) - x(im)      ! calculates the boundary value
+      dx(1) = x(1) - x(im)            ! calculates the boundary value
       dx(2:im) = x(2:im) - x(1:im-1)  ! calculates the finite diff for all other elements of x
-   end function diff
+   end function diff_upwind
+
+   pure function diff_centered(x) result(dx)
+      real(real32), intent(in) :: x(:)
+      real(real32) :: dx(size(x))
+      integer(int32) :: im
+      im = size(x)
+      dx(1) = x(2) - x(im)             ! calculates the boundary value on the left
+      dx(im) = x(1) - x(im-1)          ! calculates the boundary value on the right
+      dx(2:im-1) = x(3:im) - x(1:im-2) ! calculates the difference in the interior
+      ! divides all elements by 2
+      ! in theory, it is preferred to multiply by 0.5
+      ! bc for fp numbers, mult is faster than div.
+      dx = 0.5 * dx                    
+   end function diff_centered
 
 end module mod_diff
--- ch4/tsunami.f90
+++ ch4/tsunami.f90
@@ -1,7 +1,7 @@
 program tsunami
 
   use iso_fortran_env, only: int32, real32
-  use mod_diff, only: diff
+  use mod_diff, only: diff => diff_centered
   use mod_gaussian, only: set_gaussian
 
   ! for more functional goodness see https://wavebitscientific.github.io/functional-fortran/
@@ -11,14 +11,16 @@ program tsunami
   integer(int32) :: n                                ! loop iterations (n)
 
   integer(int32), parameter :: grid_size = 100       ! array size
-  integer(int32), parameter :: num_time_steps = 100  ! num iterations
-  real(real32), parameter :: dt = 1.                  ! time step [s]
+  integer(int32), parameter :: num_time_steps = 5000  ! num iterations
+  real(real32), parameter :: dt = 0.02                ! time step [s]
   real(real32), parameter :: dx = 1.                  ! grid spacing [m]
-  real(real32), parameter :: c = 1.                   ! phase speed [m/s]
+  real(real32), parameter :: g = 9.8                  ! gravitational acc
+  real(real32), parameter :: hmean = 10
 
   ! declares h as a real array with the number of elements equal to grid_size
   ! short-hand for `real, dimension(grid_size) :: h`
   real(real32) :: h(grid_size)  ! water height
+  real(real32) :: u(grid_size)  ! water velocity
   
   ! central index and decay factor of the water height Gaussian shape
   integer(int32), parameter :: icenter = 25
@@ -27,20 +29,17 @@ program tsunami
   if (grid_size <= 0) stop 'grid_size must be > 0'
   if (dt <= 0) stop 'time step dt must be > 0'
   if (dx <= 0) stop 'grid spacing dx must be > 0'
-  if (c <= 0) stop 'background flow speed c must be > 0'
   
-  ! initialize the water height
+  ! initialize the water height and velocity
   call set_gaussian(h, icenter, decay)
+  u = 0
 
   print *, 0, h
 
   ! iterating the solution forward in time
   time_loop: do n = 1, num_time_steps
-    ! we can substitute this one-liner in place of our looping
-    ! because h and diff(h) are of the same shape (1d) and size
-    ! computes the finite difference of water height on the fly
-    ! by calling the diff function
-    h = h - c * diff(h) / dx * dt
+    u = u - (u * diff(u) + g * diff(h)) / dx * dt ! calc water velocity
+    h = h - diff(u * (hmean + h)) / dx * dt       ! calc watr height
     print *, n, h
   end do time_loop
 
